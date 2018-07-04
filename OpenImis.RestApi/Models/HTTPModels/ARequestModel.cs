6d48f919a64340fac0feb097a29ab7b7b17972dd
Dragos DOBRE <dobre.dragos@gmail.com>
2018-07-04 17:05:53 +0200

    - changed the structure - added integration tests for LoginController - added Password utility

---
+++ OpenImis.RestApi/Models/HTTPModels/ARequestModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenImis.RestApi.Models.HTTPModels
+{
+    /// <summary>
+    /// This class serves as a parameter class for the Login call 
+    /// </summary>
+    public abstract class ARequestModel
+    {
+        private List<string> ErrorMessages { get; set; }
+
+        private Boolean IsValidCalled = false;
+        
+    }
+}
