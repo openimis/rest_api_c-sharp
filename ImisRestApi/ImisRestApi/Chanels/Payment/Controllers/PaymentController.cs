cdacb3fdc842e9da77ff22499dcc58e7a7a0fa7e
Augustino Bildad <tinodady33@yahoo.com>
2018-09-18 10:40:32 +0300

    created chf controller for payment

--- ImisRestApi/ImisRestApi/Chanels/Payment/Controllers/PaymentController.cs
+++ ImisRestApi/ImisRestApi/Chanels/Payment/Controllers/PaymentController.cs
@@ -18,36 +18,5 @@ namespace ImisRestApi.Controllers
 
         }
 
-        [HttpPost]
-        [Route("api/GetControlNumber/Single")]
-        public IActionResult Index([FromBody]IntentOfSinglePay payment)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-
-            _payment.SaveIntent(payment);
-
-            return Ok();
-        }
-
-        [HttpPost]
-        [Route("api/GetReconciliationData")]
-        public IActionResult GetReconciliation([FromBody]GepgReconcMessage model)
-        {
-            if (!ModelState.IsValid)
-                return BadRequest(ModelState);
-
-            return Ok();
-        }
-
-        public override IActionResult ReceiveControlNumber([FromBody] GepgBillResponse model)
-        {
-            return base.ReceiveControlNumber(model);
-        }
-
-        public override IActionResult GetPayment([FromBody] PaymentData model)
-        {
-            return base.GetPayment(model);
-        }
     }
 }
