2cff808fb6447a895abd7896ada65171da8bf03c
Augustino Bildad <tinodady33@yahoo.com>
2018-09-20 15:43:01 +0300

    reverted

---
+++ ImisRestApi/ImisRestApi/Chanels/Payment/Models/IntentOfSinglePay.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using ImisRestApi.Models;
+using Newtonsoft.Json;
+
+namespace ImisRestApi.Chanels
+{
+    public class IntentOfSinglePay:IntentOfPay
+    {
+        public string InsureeNumber { get; set; }
+        public string ProductCode { get; set; }
+        public EnrolmentType EnrolmentType { get; set; }
+        [JsonIgnore]
+        public override List<PaymentDetail> PaymentDetails { get => base.PaymentDetails; set => base.PaymentDetails = value; }
+    }
+}
