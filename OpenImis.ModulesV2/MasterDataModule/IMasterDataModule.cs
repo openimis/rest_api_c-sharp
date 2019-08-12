80c1d4e387e56bc56378a88a0aeb9cfe0d450274
bkaminski <bkaminski@soldevelo.com>
2019-08-12 14:47:17 +0200

    OS-54: Added support for new modules

---
+++ OpenImis.ModulesV2/MasterDataModule/IMasterDataModule.cs
@@ -0,0 +1,10 @@
+﻿using OpenImis.ModulesV2.MasterDataModule.Logic;
+
+namespace OpenImis.ModulesV2.MasterDataModule
+{
+    public interface IMasterDataModule
+    {
+        IMasterDataLogic GetMasterDataLogic();
+        IMasterDataModule SetMasterDataLogic(IMasterDataLogic masterDataLogic);
+    }
+}
---
+++ OpenImis.ModulesV2/MasterDataModule/Logic/MasterDataLogic.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OpenImis.ModulesV2.MasterDataModule.Logic
+{
+    public class MasterDataLogic : IMasterDataLogic
+    {
+        private IConfiguration _configuration;
+
+        public MasterDataLogic(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
