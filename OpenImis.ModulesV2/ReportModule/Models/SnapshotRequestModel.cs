2b71bdb2e2bd5598321db1dfbe447b779535bb41
bkaminski <bkaminski@soldevelo.com>
2019-10-18 19:02:11 +0200

    OS-68: Added endpoints to Report

---
+++ OpenImis.ModulesV2/ReportModule/Models/SnapshotRequestModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenImis.ModulesV2.ReportModule.Models
+{
+    public class SnapshotRequestModel
+    {
+        public string SnapshotDate { get; set; }
+    }
+}
