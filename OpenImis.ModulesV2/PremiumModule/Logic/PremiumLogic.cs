50ce1edc7413d8e3e3289fe3abff4a1f546ae8c2
bkaminski <bkaminski@soldevelo.com>
2019-10-19 16:02:45 +0200

    OS-69: Added endpoint in Premium Module

--- OpenImis.ModulesV2/PremiumModule/Logic/PremiumLogic.cs
+++ OpenImis.ModulesV2/PremiumModule/Logic/PremiumLogic.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
+using OpenImis.ModulesV2.PremiumModule.Models;
+using OpenImis.ModulesV2.PremiumModule.Repositories;
 
 namespace OpenImis.ModulesV2.PremiumModule.Logic
 {
@@ -6,9 +8,22 @@ namespace OpenImis.ModulesV2.PremiumModule.Logic
     {
         private IConfiguration _configuration;
 
+        protected IPremiumRepository premiumRepository;
+
         public PremiumLogic(IConfiguration configuration)
         {
             _configuration = configuration;
+
+            premiumRepository = new PremiumRepository(_configuration);
+        }
+
+        public bool Get(ReceiptRequestModel receipt)
+        {
+            bool response;
+
+            response = premiumRepository.Get(receipt);
+
+            return response;
         }
     }
 }
