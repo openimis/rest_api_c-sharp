49d401768fc7b878a0bab359fbd9f388d415394a
Seweryn Niedzielski <sniedzielski@soldevelo.com>
2021-04-09 13:38:27 +0200

    OTC-145: remove commented section in Program.cs

--- ImisRestApi/Program.cs
+++ ImisRestApi/Program.cs
@@ -13,15 +13,6 @@ namespace ImisRestApi
 {
     public class Program
     {
-        /*public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
-
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();*/
 
         public static void Main(string[] args)
         {
