c5f0ba4aae10e5a0e3e786d7315b217fccebc7d7
Seweryn Niedzielski <sniedzielski@soldevelo.com>
2021-04-09 15:31:16 +0200

    OTC-145: added lacking controllers from MV to CHF

---
+++ ImisRestApi/Controllers/FeedbackController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OpenImis.Modules;
+using OpenImis.Modules.FeedbackModule.Models;
+using OpenImis.Modules.Utils;
+using ImisRestApi.Security;
+
+namespace ImisRestApi.Controllers
+{
+    //[ApiVersion("2")]
+    [Authorize]
+    [Route("api/feedback/")]
+    //[ApiController]
+    public class FeedbackController : Controller
+    {
+        private readonly IImisModules _imisModules;
+
+        public FeedbackController(IImisModules imisModules)
+        {
+            _imisModules = imisModules;
+        }
+
+        [HasRights(Rights.ClaimFeedback)]
+        [HttpPost]
+        public IActionResult Post([FromBody] FeedbackRequest model)
+        {
+            int response;
+
+            try
+            {
+                response = _imisModules.GetFeedbackModule().GetFeedbackLogic().Post(model);
+            }
+            catch (ValidationException e)
+            {
+                return BadRequest(new { error = new { message = e.Message, value = e.Value } });
+            }
+
+            return Ok(response);
+        }
+
+        [HasRights(Rights.ClaimFeedback)]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<FeedbackResponseModel> response;
+
+            try
+            {
+                Guid userUUID = Guid.Parse(HttpContext.User.Claims.Where(w => w.Type == "UserUUID").Select(x => x.Value).FirstOrDefault());
+
+                Repository rep = new Repository();
+                string officerCode = rep.GetLoginNameByUserUUID(userUUID);
+
+                response = _imisModules.GetFeedbackModule().GetFeedbackLogic().Get(officerCode);
+            }
+            catch (ValidationException e)
+            {
+                return BadRequest(new { error = new { message = e.Message, value = e.Value } });
+            }
+
+            return Ok(response);
+        }
+    }
+}
---
+++ ImisRestApi/Controllers/PremiumController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OpenImis.Modules;
+using OpenImis.Modules.PremiumModule.Models;
+using ImisRestApi.Security;
+
+namespace ImisRestApi.Controllers.V2
+{
+    //[ApiVersion("2")]
+    [Authorize]
+    [Route("api/premium/")]
+    //[ApiController]
+    public class PremiumController : Controller
+    {
+        private readonly IImisModules _imisModules;
+
+        public PremiumController(IImisModules imisModules)
+        {
+            _imisModules = imisModules;
+        }
+
+        [HasRights(Rights.PolicySearch)]
+        [Route("receipt/")]
+        [HttpPost]
+        public IActionResult Get([FromBody] ReceiptRequestModel receipt)
+        {
+            bool response;
+
+            try
+            {
+                response = _imisModules.GetPremiumModule().GetPremiumLogic().Get(receipt);
+            }
+            catch (ValidationException e)
+            {
+                return BadRequest(new { error = new { message = e.Message, value = e.Value } });
+            }
+
+            return Ok(response);
+        }
+    }
+}
