c9c89085028423ede8a017c99ca7de112c5f1da6
Hiren Soni <hirensoni913@gmail.com>
2021-09-07 16:50:12 +0300

    Fixed missing data model and code for Policy Notification in V3

---
+++ OpenImis.ModulesV3/InsureeModule/Models/FamilySMS.cs
@@ -0,0 +1,31 @@
+﻿using OpenImis.DB.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenImis.ModulesV3.InsureeModule.Models
+{
+    public class FamilySMS
+    {
+        public int FamilyId { get; set; }
+        public bool ApprovalOfSMS { get; set; }
+        public string LanguageOfSMS { get; set; }
+
+        public static FamilySMS FromTblFamilySMS(TblFamilySMS tblFamilySMS)
+        {
+            if (tblFamilySMS == null)
+            {
+                return null;
+            }
+
+            FamilySMS familySMS = new FamilySMS()
+            {
+                FamilyId = tblFamilySMS.FamilyId,
+                ApprovalOfSMS = tblFamilySMS.ApprovalOfSMS,
+                LanguageOfSMS = tblFamilySMS.LanguageOfSMS
+            };
+
+            return familySMS;
+        }
+    }
+}
