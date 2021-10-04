a17ba1ad7155caf11f1033e03870317285b6ca20
Hiren Soni <hirensoni913@gmail.com>
2021-10-04 17:10:31 +0300

    Error Enum added

---
+++ OpenImis.RestApi/Errors.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenImis.RestApi
+{
+    public class Errors
+    {
+        // Enrolment Errors
+
+        // Claim Errors
+        enum Claim
+        {
+            Success = 2001,
+            InvalidHFCode,
+            DuplicateClaimCode,
+            InvalidInsuranceNumber,
+            EndDateIsBeforeStartDate,
+            InvalidICDCode,
+            InvalidItem,
+            InvalidService,
+            InvalidClaimAdmin
+        }
+    }
+}
