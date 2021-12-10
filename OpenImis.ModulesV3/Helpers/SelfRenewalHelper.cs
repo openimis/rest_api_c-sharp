ebcc74308ac20f86efe10bb2ad2f4ad1db9e12c5
Hiren Soni <hirensoni913@gmail.com>
2021-12-10 16:53:12 +0300

    New error codes added for the renewal

---
+++ OpenImis.ModulesV3/Helpers/SelfRenewalHelper.cs
@@ -0,0 +1,21 @@
+﻿using OpenImis.ModulesV3.PolicyModule.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenImis.ModulesV3.Helpers
+{
+    class SelfRenewalHelper
+    {
+        public int CreateSelfRenewal(SelfRenewal renewal)
+        {
+
+            return 0;
+        }
+
+        private int Validate(SelfRenewal renewal)
+        {
+
+        }
+    }
+}
