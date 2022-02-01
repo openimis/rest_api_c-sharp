787577a52fffdc0954596f0cea3edabf06373c2f
Hiren Soni <hirensoni913@gmail.com>
2022-02-01 11:07:22 +0300

    Header section added in BulkControlNumbersForEO

--- OpenImis.ePayment/Escape/Payment/Models/BulkControlNumbersForEO.cs
+++ OpenImis.ePayment/Escape/Payment/Models/BulkControlNumbersForEO.cs
@@ -6,6 +6,18 @@ namespace OpenImis.ePayment.Escape.Payment.Models
 {
     public class BulkControlNumbersForEO
     {
+        public Header Header { get; set; }
+        public List<ControlNumbers> ControlNumbers { get; set; }
+    }
+
+    public class Header
+    {
+        public int Error { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+
+    public class ControlNumbers
+    {
         public int ControlNumberId { get; set; }
         public int BillId { get; set; }
         public string ProductCode { get; set; }
