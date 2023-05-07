06eeb3c755d20eec7de22e87f464e30d204a1291
maxime ngoe <maxime.ngoe@gmail.com>
2023-05-07 16:26:41 +0100

    ajustement Attachment

---
+++ OpenImis.ModulesV3/InsureeModule/Models/EnrollFamilyModels/Attachment.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenImis.ModulesV3.InsureeModule.Models.EnrollFamilyModels
+{
+    public class Attachment
+    {
+        public string Title { get; set; }
+        public string Filename { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ OpenImis.ModulesV3/InsureeModule/Models/EnrollFamilyModels/Attachments.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Attachment
+
+namespace OpenImis.ModulesV3.InsureeModule.Models.EnrollFamilyModels
+{
+    public class Attachments
+    {
+        public Attachment attachment { get; set; }
+    }
+}
